5a7a7b1b4774d7da92153e2ef4855283bde76a96
Christopher Adams <christopher@fabricatorz.com>
2016-07-28 13:55:00 +0800

    Fix indentation

--- lib/my_app/hello_resource.ex
+++ lib/my_app/hello_resource.ex
@@ -2,14 +2,14 @@ defmodule MyApp.HelloResource do
   @behaviour PlugRest.Resource
 
   def allowed_methods(conn, state) do
-      {["GET"], conn, state}
+    {["GET"], conn, state}
   end
 
   def content_types_provided(conn, state) do
-      {[{"text/html", :to_html}], conn, state}
+    {[{"text/html", :to_html}], conn, state}
   end
 
   def to_html(conn, state) do
-      {"Hello world", conn, state}
+    {"Hello world", conn, state}
   end
 end
