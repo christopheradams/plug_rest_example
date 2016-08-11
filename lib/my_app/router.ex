3dc833c1445e075f303fb4a6d1f7799eefe3be10
Christopher Adams <christopher@fabricatorz.com>
2016-08-11 17:25:27 +0800

    Move ErrorHandler after Debugger

--- lib/my_app/router.ex
+++ lib/my_app/router.ex
@@ -1,10 +1,11 @@
 defmodule MyApp.Router do
   use PlugRest.Router
-  use Plug.ErrorHandler
 
   if Mix.env == :dev do
     use Plug.Debugger, otp_app: :my_app
   end
+
+  use Plug.ErrorHandler
 
   resource "/hello", MyApp.HelloResource
   resource "/users/:username", MyApp.UserResource
