82ed9c7b6b42ef2bd2a177f1477eeaafaaef3a04
Christopher Adams <christopher@fabricatorz.com>
2016-09-02 10:31:40 +0800

    Switch to local copy plug_rest

--- mix.exs
+++ mix.exs
@@ -18,6 +18,6 @@ defmodule MyApp.Mixfile do
   defp deps do
     [{:cowboy, "~> 1.0.0"},
      {:plug, "~> 1.0"},
-     {:plug_rest, "~> 0.8.0"}]
+     {:plug_rest, path: "/home/christopher/Documents/Repositories/github.com/christopheradams/plug_rest"}]
   end
 end
